297fa4d6e9c50f768a906f0a351b757777036ffc
Oswin Bult <obult@student.codam.nl>
2022-08-13 17:04:34 +0200

    BrainStart

---
+++ ex01/Brain.cpp
@@ -0,0 +1,24 @@
+#include <Brain.hpp>
+
+#include <string>
+#include <iostream>
+
+Brain::Brain()
+{
+
+}
+
+Brain::Brain(const Brain& ref)
+{
+
+}
+
+Brain&	Brain::operator=(const Brain& ref)
+{
+
+}
+
+Brain::~Brain()
+{
+
+}
---
+++ ex01/Brain.hpp
@@ -0,0 +1,13 @@
+#ifndef BRAIN_HPP
+# define BRAIN_HPP
+
+class	Brain
+{
+public:
+	Brain();
+	Brain(const Brain& ref);
+	Brain& operator=(const Brain& ref);
+	~Brain();
+};
+
+#endif
