8be22ad2a4972ff429f653aa9d8959754bba0b45
Oswin Bult <obult@student.codam.nl>
2022-08-13 20:24:33 +0200

    more tests

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -5,29 +5,34 @@
 int main()
 {
     const Animal* meta = new Animal();
-    const Animal* j = new Dog();
-    const Animal* i = new Cat();
-    std::cout << j->getType() << " " << std::endl;
-    std::cout << i->getType() << " " << std::endl;
-    i->makeSound();
-    j->makeSound();
+    const Animal* d = new Dog();
+    const Animal* c = new Cat();
+    std::cout << d->getType() << " " << std::endl;
+    std::cout << c->getType() << " " << std::endl;
+    c->makeSound();
+    d->makeSound();
     meta->makeSound();
 
     std::cout << std::endl;
 
-    j->setIdea(0, "snappen mensen iets van die geluiden die ze naar elkaar maken?");
-    std::cout << j->getIdea(0) << std::endl;
+    d->setIdea(0, "snappen mensen iets van die geluiden die ze naar elkaar maken?");
+    std::cout << d->getIdea(0) << std::endl;
+
+    Dog precopy;
+    precopy.setIdea(0, "mijn eigen gedachte");
+    const Animal* copy = new Dog(precopy);
+    std::cout << copy->getIdea(0) << std::endl;
+
     try
     {
-        std::cout << j->getIdea(200) << std::endl;
+        std::cout << d->getIdea(200) << std::endl;
     }
     catch(const std::exception& e)
     {
         std::cerr << e.what() << std::endl;
     }
-    
 
     delete meta;
-    delete i;
-    delete j;
+    delete c;
+    delete d;
 }
